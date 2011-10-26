f772f68c718a38fa3bc4fb4a0a02d44220581823
Mike Cook <[omitted]>
2011-10-27 00:12:35 +0100

    setup validation messages (passed/failed

--- lib/epub_validator/check_epub.rb
+++ lib/epub_validator/check_epub.rb
@@ -6,20 +6,27 @@ class EpubValidator::CheckEpub
   def validate_epub_file
     epubcheck_jar = 'lib/epubcheck-1.2/epubcheck-1.2.jar'
     message = `java -jar #{epubcheck_jar} "#{@filename}" 2>&1`
-    format_message(message)
+
+    if message.match('No errors or warnings detected')
+      'Passed.'
+    else
+      format_failed_validation_message(message)
+    end
   end
 
   private
 
-  def format_message(message)
-    messages_array = message.split(/\n/)
+  def format_failed_validation_message(message)
+    m_array = message.split(/\n/)
 
     # clean up all useless info
-    messages_array.delete_if do |s|
+    m_array.delete_if do |s|
       s.empty? or
-      s.match('^Epubcheck Version.*') or
-      s.match('^Check finished.*')
+        s.match('^Epubcheck Version.*') or
+        s.match('^Check finished.*')
     end
+
+    m_array.unshift('FAILED!')
   end
 end
 
