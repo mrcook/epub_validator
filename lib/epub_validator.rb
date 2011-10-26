40372510d5641f680940c0031b13370dc6a74144
Mike Cook <[omitted]>
2011-10-26 20:50:21 +0100

    move CheckEpub class to its own file

--- lib/epub_validator.rb
+++ lib/epub_validator.rb
@@ -1,24 +1,6 @@
 Dir["#{File.dirname(__FILE__)}/epub_validator/**/*"].each {|file| require(file)}
 
+require 'epub_validator/check_epub'
+
 module EpubValidator
-  class CheckEpub
-    attr_accessor :filename
-
-    def initialize(filename)
-      @filename = filename
-    end
-
-    def get_epubcheck_output
-      results = `java -jar epubcheck-1.2/epubcheck-1.2.jar "#{@filename}" 2>&1`
-      #results = external.split("\n")
-
-      ## clean up all useless info
-      #results.delete_if do |s|
-      #s.empty? or
-      #s.match('^Epubcheck Version.*') or
-      #s.match('^No errors.*') or
-      #s.match('^Check finished.*')
-      #end
-    end
-  end
 end
---
+++ lib/epub_validator/check_epub.rb
@@ -0,0 +1,19 @@
+class EpubValidator::CheckEpub
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def get_epubcheck_output
+    results = `java -jar epubcheck-1.2/epubcheck-1.2.jar "#{@filename}" 2>&1`
+    #results = external.split("\n")
+
+    ## clean up all useless info
+    #results.delete_if do |s|
+    #s.empty? or
+    #s.match('^Epubcheck Version.*') or
+    #s.match('^No errors.*') or
+    #s.match('^Check finished.*')
+    #end
+  end
+end
+
