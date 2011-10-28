e37128a1aaa2b97a24820cf7c7b6d6e95adc0a14
Mike Cook <[omitted]>
2011-10-29 00:09:47 +0100

    rubgems complaining so created module EpubValidator in epub_check

--- lib/epub_validator.rb
+++ lib/epub_validator.rb
@@ -3,8 +3,8 @@ Dir["#{File.dirname(__FILE__)}/epub_validator/**/*"].each {|file| require(file)}
 require 'epub_validator/check_epub'
 
 module EpubValidator
-    def self.check_file(filename)
-      CheckEpub.new(filename)
-    end
+  def self.check_file(filename)
+    CheckEpub.new(filename)
+  end
 end
 
--- lib/epub_validator/check_epub.rb
+++ lib/epub_validator/check_epub.rb
@@ -1,29 +1,30 @@
-class EpubValidator::CheckEpub
-  attr_accessor :message
+module EpubValidator
+  class CheckEpub
+    attr_accessor :message
 
-  def initialize(filename)
-    @filename = filename
-    @message = format_epubcheck_message(process_epub)
-  end
-
-  def process_epub
-    epubcheck_jar = 'lib/epubcheck-1.2/epubcheck-1.2.jar'
-    epubcheck = `java -jar #{epubcheck_jar} "#{@filename}" 2>&1`
-  end
-
-  def format_epubcheck_message(message)
-    return ['Passed.'] if message.match('No errors or warnings detected')
-
-    m_array = message.split(/\n/)
-
-    # clean up all useless info
-    m_array.delete_if do |s|
-      s.empty? or
-        s.match('^Epubcheck Version.*') or
-        s.match('^Check finished.*')
+    def initialize(filename)
+      @filename = filename
+      @message = format_epubcheck_message(process_epub)
     end
 
-    m_array.unshift('FAILED!')
+    def process_epub
+      epubcheck_jar = 'lib/epubcheck-1.2/epubcheck-1.2.jar'
+      epubcheck = `java -jar #{epubcheck_jar} "#{@filename}" 2>&1`
+    end
+
+    def format_epubcheck_message(message)
+      return ['Passed.'] if message.match('No errors or warnings detected')
+
+      m_array = message.split(/\n/)
+
+      # clean up all useless info
+      m_array.delete_if do |s|
+        s.empty? or
+          s.match('^Epubcheck Version.*') or
+          s.match('^Check finished.*')
+      end
+
+      m_array.unshift('FAILED!')
+    end
   end
 end
-
