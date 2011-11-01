e49fd6c3cfd1ec7c05d68050063775f692c3499e
Mike Cook <m@mikecook.co.uk>
2011-11-01 15:47:10 +0000

    removed pending test

--- lib/epub_validator.rb
+++ lib/epub_validator.rb
@@ -3,8 +3,8 @@ Dir["#{File.dirname(__FILE__)}/epub_validator/**/*"].each {|file| require(file)}
 require 'epub_validator/check_epub'
 
 module EpubValidator
-  def self.check_file(filename)
-    EpubCheck.new(filename)
+  def self.run(filename)
+    EpubCheck.process(filename)
   end
 end
 
