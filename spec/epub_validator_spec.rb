ea4a5d5bdb624ddd591a2cdac4d6bcca793cd7f5
Mike Cook <m@mikecook.co.uk>
2011-11-01 15:49:18 +0000

    removed pending test

--- spec/epub_validator_spec.rb
+++ spec/epub_validator_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe EpubValidator do
   context "when given a single EPUB filename" do
-    it "should validate the file"
+    #it "should validate the file"
   end
 end
