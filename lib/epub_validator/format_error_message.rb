ebe54bac534697095c34289d228d686ae59a4183
Mike Cook <m@mikecook.co.uk>
2011-11-17 20:44:50 +0000

    moved message formatting to separate Class

---
+++ lib/epub_validator/format_error_message.rb
@@ -0,0 +1,18 @@
+module EpubValidator
+  class FormatErrorMessage
+    def process_message(message)
+      return ['Passed.'] if message.match('No errors or warnings detected')
+
+      m_array = message.split(/\n/)
+
+      # clean up all useless info
+      m_array.delete_if do |s|
+        s.empty? or
+          s.match('^Epubcheck Version.*') or
+          s.match('^Check finished.*')
+      end
+
+      m_array.unshift('FAILED!')
+    end
+  end
+end
