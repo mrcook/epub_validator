c64ae341257fdb066a7a7641d228b640c77f6b68
Mike Cook <m@mikecook.co.uk>
2011-11-17 20:10:17 +0000

    changed module method from 'run' to 'check'.

--- lib/epub_validator.rb
+++ lib/epub_validator.rb
@@ -1,9 +1,9 @@
 Dir["#{File.dirname(__FILE__)}/epub_validator/**/*"].each {|file| require(file)}
 
-require 'epub_validator/check_epub'
+require 'epub_validator/epub_check'
 
 module EpubValidator
-  def self.run(filename)
+  def self.check(filename)
     EpubCheck.process(filename)
   end
 end
