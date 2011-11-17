0e5fac7e5f76df3f12577a45d975eeaba36f9335
Mike Cook <m@mikecook.co.uk>
2011-11-17 20:44:39 +0000

    moved message formatting to separate Class

--- lib/epub_validator/epub_check.rb
+++ lib/epub_validator/epub_check.rb
@@ -1,30 +1,8 @@
 module EpubValidator
   class EpubCheck
-    def self.process(filename)
-      checked = epubcheck(filename)
-      format_epubcheck_message(checked)
-    end
-
-    private
-
-    def self.epubcheck(epubfile)
+    def self.epubcheck(filename)
       epubcheck_jar = File.expand_path(File.dirname(__FILE__) + '/../epubcheck-1-2/epubcheck-1.2.jar')
       `java -jar #{epubcheck_jar} "#{epubfile}" 2>&1`
-    end
-
-    def self.format_epubcheck_message(message)
-      return ['Passed.'] if message.match('No errors or warnings detected')
-
-      m_array = message.split(/\n/)
-
-      # clean up all useless info
-      m_array.delete_if do |s|
-        s.empty? or
-          s.match('^Epubcheck Version.*') or
-          s.match('^Check finished.*')
-      end
-
-      m_array.unshift('FAILED!')
     end
   end
 end
--- spec/epub_validator/epub_check_spec.rb
+++ spec/epub_validator/epub_check_spec.rb
@@ -2,30 +2,12 @@ require 'spec_helper'
 
 module EpubValidator
   describe EpubCheck do
-    context "when it tries to process a missing file" do
-      it "should return a formatted I/O error message" do
-        message = "ERROR: test.epub: I/O error: test.epub (No such file or directory)"
-        formatted_message = ["FAILED!", "ERROR: test.epub: I/O error: test.epub (No such file or directory)"]
-        EpubCheck.stub(:epubcheck).and_return(message)
-        ec = EpubCheck.process("test.epub")
-        ec.should eq(formatted_message)
-      end
-    end
-    context "when it processes a valid file" do
-      it "should return 'Passed.' message as an array" do
-        message = "Epubcheck Version 1.2\n\nNo errors or warnings detected\n"
-        EpubCheck.stub(:epubcheck).and_return(message)
-        ec = EpubCheck.process("test.epub")
-        ec.should eq(['Passed.'])
-      end
-    end
-    context "when it processes an invalid file" do
-      it "should return error message as an array" do
+    context "when it processes a file" do
+      it "should return a message" do
         message = "Epubcheck Version 1.2\n\nERROR: book.epub: resource OEBPS/stylesheets/handbookish.css is missing\n\nCheck finished with warnings or errors!"
-        formatted_message = ["FAILED!", "ERROR: book.epub: resource OEBPS/stylesheets/handbookish.css is missing"]
         EpubCheck.stub(:epubcheck).and_return(message)
-        ec = EpubCheck.process("test.epub")
-        ec.should eq(formatted_message)
+        ec = EpubCheck.epubcheck("test.epub")
+        ec.should eq(message)
       end
     end
   end
