00d20a7370bcf57ecef52ce2c033dc4cb6e71f41
Mike Cook <mike@mikecook.co.uk>
2012-01-08 18:25:55 +0000

    made tests pass for new API

--- spec/epub_validator/format_message_spec.rb
+++ spec/epub_validator/format_message_spec.rb
@@ -2,27 +2,33 @@ require 'spec_helper'
 
 module EpubValidator
   describe FormatMessage do
-    context "when it recieves a missing file message" do
-      it "should return a 'FAILED!' message as an array" do
+    context "when it receives a missing file message" do
+      it "should return a failed hash message" do
         message = "ERROR: test.epub: I/O error: test.epub (No such file or directory)"
-        formatted_message = ["FAILED!", "ERROR: test.epub: I/O error: test.epub (No such file or directory)"]
+        formatted_message = ["#{message}"]
+
         errm = FormatMessage.new
-        errm.process_message(message).should eq(formatted_message)
+        m = errm.process_message(message)
+        m[:valid].should eq(0)
+        m[:message].should eq(formatted_message)
       end
     end
-    context "when it recieves a valid file message" do
-      it "should return 'Passed.' message as an array" do
+    context "when it receives a valid file message" do
+      it "should return :valid => 1" do
         message = "Epubcheck Version 1.2\n\nNo errors or warnings detected\n"
         errm = FormatMessage.new
-        errm.process_message(message).should eq(['Passed.'])
+        errm.process_message(message).should eq(:valid => 1)
       end
     end
     context "when it recieves an invalid file message" do
       it "should return error message as an array" do
         message = "Epubcheck Version 1.2\n\nERROR: book.epub: resource OEBPS/stylesheets/handbookish.css is missing\n\nCheck finished with warnings or errors!"
-        formatted_message = ["FAILED!", "ERROR: book.epub: resource OEBPS/stylesheets/handbookish.css is missing"]
+        formatted_message = ["ERROR: book.epub: resource OEBPS/stylesheets/handbookish.css is missing"]
+
         errm = FormatMessage.new
-        errm.process_message(message).should eq(formatted_message)
+        m = errm.process_message(message)
+        m[:valid].should eq(0)
+        m[:message].should eq(formatted_message)
       end
     end
   end
--- spec/epub_validator/process_epub_spec.rb
+++ spec/epub_validator/process_epub_spec.rb
@@ -11,9 +11,12 @@ module EpubValidator
       end
       it "should return the output message formatted" do
         message = "Epubcheck Version 1.2\n\nERROR: book.epub: resource OEBPS/stylesheets/handbookish.css is missing\n\nCheck finished with warnings or errors!"
-        formatted_message = ["FAILED!", "ERROR: book.epub: resource OEBPS/stylesheets/handbookish.css is missing"]
+        formatted_message = ["ERROR: book.epub: resource OEBPS/stylesheets/handbookish.css is missing"]
+
         ec = ProcessEpub.new
-        ec.format_epubcheck_output(message).should eq(formatted_message)
+        m = ec.format_epubcheck_output(message)
+        m[:valid].should eq(0)
+        m[:message].should eq(formatted_message)
       end
     end
   end
