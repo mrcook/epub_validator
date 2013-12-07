ffa5a1a38d3e38ab4517adc3f0e03c75426bbb58
Mike Cook <m@mikecook.co.uk>
2013-12-07 11:34:41 +0000

    Update epubcheck to latest 3.0.1 version

--- lib/epub_validator/process_epub.rb
+++ lib/epub_validator/process_epub.rb
@@ -1,7 +1,7 @@
 module EpubValidator
   class ProcessEpub
     def epubcheck(filename)
-      epubcheck_jar = File.expand_path(File.dirname(__FILE__) + '/../epubcheck-3-0/epubcheck-3.0.jar')
+      epubcheck_jar = File.expand_path(File.dirname(__FILE__) + '/../epubcheck-3.0.1/epubcheck-3.0.1.jar')
       format_epubcheck_output(`java -jar #{epubcheck_jar} "#{filename}" 2>&1`)
     end
 
